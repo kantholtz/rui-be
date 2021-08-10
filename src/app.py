210e400740de78b68f845646f1ed6461141553b6
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-08-10 17:09:25 +0200

    Parse Match TXT and refactor put_data()

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Dict, Tuple, List
+from typing import Iterator
 
 import yaml
 from draug.homag.graph import RELATIONS, Graph
@@ -43,19 +43,114 @@ def get_root():
 class Entity:
     id: int
     parent: int
-    names: List[str]
+    names: list[str]
 
 
 @dataclass
 class DeepEntity:
     id: int
     parent: int
-    names: List[str]
-    children: List
+    names: list[str]
+    children: list
 
 
-@app.route('/api/1.1.0/taxonomy', methods=['GET'])
-def get_taxonomy() -> Dict[str, List[DeepEntity]]:
+@dataclass
+class Phrase:
+    id: int
+    text: str
+
+
+@dataclass
+class Text:
+    label: str
+    mention: str
+    ticket: int
+    phrase: Phrase
+
+
+@app.route('/api/1.2.0/upload', methods=['PUT'])
+def put_data() -> str:
+    global graph
+
+    meta_yml: FileStorage = request.files['metaYml']
+    nodes_txt: FileStorage = request.files['nodesTxt']
+    edges_txt: FileStorage = request.files['edgesTxt']
+    match_txt: FileStorage = request.files['matchTxt']
+
+    meta = yaml.load(meta_yml.stream, Loader=yaml.FullLoader)
+    nodes = parse_nodes_txt(nodes_txt)
+    edges = parse_edges_txt(edges_txt)
+
+    graph = Graph.load_from_memory(meta, nodes, edges)
+
+    matches = parse_match_txt(match_txt)
+
+    return ''
+
+
+def parse_nodes_txt(nodes_txt: FileStorage) -> Iterator[tuple[int, dict]]:
+    """
+    Parse Nodes TXT whose lines have the following format:
+
+    node {data} ...
+    """
+
+    def parse_line(line: str) -> tuple[int, dict]:
+        chunks = line.split(' ', maxsplit=1)
+
+        node_id = int(chunks[0])
+        data = eval(chunks[1])
+
+        return node_id, data
+
+    return (parse_line(str(line)) for line in nodes_txt.stream)
+
+
+def parse_edges_txt(edges_txt: FileStorage) -> Iterator[tuple[int, int, int]]:
+    """
+    Parse Edges TXT whose lines have the following format:
+
+    head tail rel
+    """
+
+    def parse_line(line: str) -> tuple[int, int, int]:
+        chunks = line.split(' ')
+
+        head = int(chunks[0])
+        tail = int(chunks[1])
+        rel = int(chunks[2])
+
+        return head, tail, rel
+
+    return (parse_line(str(line)) for line in edges_txt.stream)
+
+
+def parse_match_txt(match_txt: FileStorage) -> Iterator[tuple[str, str, int, int, str]]:
+    """
+    Parse Nodes TXT whose lines have the following format:
+
+    entity_label|mention|ticket.phrase_id|phrase_text
+    """
+
+    def parse_line(line: str) -> tuple[str, str, int, int, str]:
+        chunks = line.split('|')
+
+        entity = str(chunks[0])
+        mention = str(chunks[1])
+
+        ticket_chunk, phrase_id_chunk = chunks[2].split('.')
+        ticket = int(ticket_chunk)
+        phrase_id = int(phrase_id_chunk)
+
+        phrase_text = str(chunks[3])
+
+        return entity, mention, ticket, phrase_id, phrase_text
+
+    return (parse_line(str(line)) for line in match_txt.stream)
+
+
+@app.route('/api/1.2.0/taxonomy', methods=['GET'])
+def get_taxonomy() -> dict[str, list[DeepEntity]]:
     root_entity_ids = graph.find_root_ents()
 
     #
@@ -64,61 +159,40 @@ def get_taxonomy() -> Dict[str, List[DeepEntity]]:
 
     def id_to_entity(entity_id: int) -> DeepEntity:
         return DeepEntity(id=entity_id,
-                           parent=graph.get_parent(entity_id),
-                           names=graph.nxg.nodes[entity_id]['names'],
-                           children=[id_to_entity(child) for child in graph.get_children(entity_id)])
+                          parent=graph.get_parent(entity_id),
+                          names=graph.nxg.nodes[entity_id]['names'],
+                          children=[id_to_entity(child) for child in graph.get_children(entity_id)])
 
     return {'taxonomy': [id_to_entity(root_node) for root_node in root_entity_ids]}
 
 
-@app.route('/api/1.1.0/taxonomy', methods=['PUT'])
-def put_taxonomy() -> str:
-    global graph
-
-    meta_yml: FileStorage = request.files['metaYml']
-    nodes_txt: FileStorage = request.files['nodesTxt']
-    edges_txt: FileStorage = request.files['edgesTxt']
-
-    meta_dict = yaml.load(meta_yml.stream, Loader=yaml.FullLoader)
-
-    str_nodes = (line.split(b' ', maxsplit=1) for line in nodes_txt.stream)
-    nodes = ((int(node_id), eval(data)) for node_id, data in str_nodes)
-
-    str_triples = (line.split() for line in edges_txt.stream)
-    triples = ((int(str_triple[0]), int(str_triple[1]), int(str_triple[2])) for str_triple in str_triples)
-
-    graph = Graph.load_from_memory(meta_dict, nodes, triples)
-
-    return ''
-
-
-@app.route('/api/1.1.0/entity', methods=['POST'])
-def post_entity() -> Tuple[str, int]:
-    request_data: Dict = request.get_json()
+@app.route('/api/1.2.0/entity', methods=['POST'])
+def post_entity() -> tuple[str, int]:
+    request_data: dict = request.get_json()
 
     entity = Entity(id=request_data['id'],
-                      parent=request_data['parent'],
-                      names=request_data['names'])
+                    parent=request_data['parent'],
+                    names=request_data['names'])
 
     graph.add_ent(entity.parent, entity.names)
 
     return '', 201
 
 
-@app.route('/api/1.1.0/entity', methods=['PUT'])
+@app.route('/api/1.2.0/entity', methods=['PUT'])
 def put_entity() -> str:
-    request_data: Dict = request.get_json()
+    request_data: dict = request.get_json()
 
     entity = Entity(id=request_data['id'],
-                      parent=request_data['parent'],
-                      names=request_data['names'])
+                    parent=request_data['parent'],
+                    names=request_data['names'])
 
     graph.update_ent(entity.id, entity.parent, entity.names)
 
     return ''
 
 
-@app.route('/api/1.1.0/entity/<int:entity_id>', methods=['DELETE'])
+@app.route('/api/1.2.0/entity/<int:entity_id>', methods=['DELETE'])
 def delete_entity(entity_id: int) -> str:
     graph.delete_ent(entity_id)
 
