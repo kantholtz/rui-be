daeb4d023066e5e80b390245e936f402dd3e6821
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-09-15 13:52:53 +0200

    Switch from distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='rui-be',
-    version='0.8.0'
+    version='0.8.0',
+
+    install_requires=[
+        'Flask~=2.0.1'
+    ],
+
+    extras_require={
+        'dev': [
+            'pytest'
+        ]
+    }
 )
