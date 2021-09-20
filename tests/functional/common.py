55fdf12589481e67da6fd23044719126860d98fc
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-09-20 14:19:20 +0200

    Update test data

--- tests/functional/common.py
+++ tests/functional/common.py
@@ -1,6 +1,6 @@
 def upload(client):
-    with open('fixtures/symptax_upload_v6_2_random.zip', 'rb') as fd:
-        data = {'symptaxUploadZip': (fd, 'symptax_upload_v6_2_random.zip')}
+    with open('fixtures/symptax_upload_v7_random.zip', 'rb') as fd:
+        data = {'symptaxUploadZip': (fd, 'symptax_upload_v7_random.zip')}
 
         response = client.put('http://localhost:5000/api/1.6.0/upload',
                               content_type='multipart/form-data',
