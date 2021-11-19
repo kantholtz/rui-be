26fc05f55c1d73afdfbaf17279326ef98df9576a
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-11-19 12:23:27 +0100

    Separate test_post_root_node() and test_post_child_node() and simplify tests

--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -27,24 +27,14 @@ def test_get_nodes(client):
     DeepNodeSchema(many=True).load(nodes)
 
 
-def test_post_node(client):
+def test_post_root_node(client):
     ### GIVEN   a backend with data
 
     upload(client)
 
-    ### WHEN    posting a node
+    ### WHEN    posting a root node
 
-    post_root_node = PostNode(
-        parent_id=None,
-        entities=[
-            PostNodeEntity('D-1'),
-            PostNodeEntity('D-2')
-        ]
-    )
-
-    post_root_node = PostNodeSchema().dump(post_root_node)
-
-    expected_post_root_node = {
+    post_root_node = {
         'parentId': None,
         'entities': [
             {'name': 'D-1'},
@@ -52,205 +42,70 @@ def test_post_node(client):
         ]
     }
 
-    assert post_root_node == expected_post_root_node
-
     post_root_node_response = client.post('http://localhost:5000/api/1.6.0/nodes', json=post_root_node)
+
+    ### THEN    the backend should respond with an HTTP 201
 
     assert post_root_node_response.status_code == 201
 
-    #
-    # POST /nodes
-    #
+    ### THEN    the backend should have persisted the root node
 
-    post_child_node = PostNode(
-        parent_id=0,
-        entities=[
-            PostNodeEntity('Ac-1')
-        ]
-    )
+    get_response = client.get('http://localhost:5000/api/1.6.0/nodes')
+    nodes = get_response.get_json()
 
-    post_child_node = PostNodeSchema().dump(post_child_node)
+    expected_node_d = {
+        'id': 7,
+        'parentId': None,
+        'entities': [
+            {'id': 12, 'nodeId': 7, 'name': 'D-1', 'matchesCount': 0},
+            {'id': 13, 'nodeId': 7, 'name': 'D-2', 'matchesCount': 0}
+        ],
+        'children': []
+    }
 
-    expected_post_child_node = {
+    expected_nodes = [node_a, node_b, node_c, expected_node_d]
+
+    assert ordered(nodes) == ordered(expected_nodes)
+
+
+def test_post_child_node(client):
+    ### GIVEN   a backend with data
+
+    upload(client)
+
+    ### WHEN    posting a child node
+
+    post_child_node = {
         'parentId': 0,
         'entities': [{'name': 'Ac-1'}]
     }
 
-    assert post_child_node == expected_post_child_node
-
     post_child_node_response = client.post('http://localhost:5000/api/1.6.0/nodes', json=post_child_node)
+
+    ### THEN    the backend should respond with an HTTP 201
 
     assert post_child_node_response.status_code == 201
 
-    #
-    # GET /nodes
-    #
+    ### THEN    the backend should have persisted the root node
 
     get_response = client.get('http://localhost:5000/api/1.6.0/nodes')
-
     nodes = get_response.get_json()
 
-    expected_nodes = [
-        {
-            'id': 0,
-            'parentId': None,
-            'entities': [
-                {
-                    'id': 0,
-                    'nodeId': 0,
-                    'name': 'A-1',
-                    'matchesCount': 2
-                }
-            ],
-            'children': [
-                {
-                    'id': 1,
-                    'parentId': 0,
-                    'entities': [
-                        {
-                            'id': 1,
-                            'nodeId': 1,
-                            'name': 'Aa-1',
-                            'matchesCount': 2
-                        }
-                    ],
-                    'children': []
-                },
-                {
-                    'id': 2,
-                    'parentId': 0,
-                    'entities': [
-                        {
-                            'id': 2,
-                            'nodeId': 2,
-                            'name': 'Ab-1',
-                            'matchesCount': 2
-                        }
-                    ],
-                    'children': []
-                },
-                {
-                    'id': 8,
-                    'parentId': 0,
-                    'entities': [
-                        {
-                            'id': 14,
-                            'nodeId': 8,
-                            'name': 'Ac-1',
-                            'matchesCount': 0
-                        }
-                    ],
-                    'children': []
-                }
-            ]
-        },
-        {
-            'id': 3,
-            'parentId': None,
-            'entities': [
-                {
-                    'id': 3,
-                    'nodeId': 3,
-                    'name': 'B-1',
-                    'matchesCount': 1
-                }
-            ],
-            'children': [
-                {
-                    'id': 4,
-                    'parentId': 3,
-                    'entities': [
-                        {
-                            'id': 4,
-                            'nodeId': 4,
-                            'name': 'Ba-1',
-                            'matchesCount': 1
-                        },
-                        {
-                            'id': 5,
-                            'nodeId': 4,
-                            'name': 'Ba-2',
-                            'matchesCount': 1
-                        },
-                        {
-                            'id': 6,
-                            'nodeId': 4,
-                            'name': 'Ba-3',
-                            'matchesCount': 0
-                        },
-                        {
-                            'id': 7,
-                            'nodeId': 4,
-                            'name': 'Ba-4',
-                            'matchesCount': 1
-                        },
-                        {
-                            'id': 8,
-                            'nodeId': 4,
-                            'name': 'Ba-5',
-                            'matchesCount': 1
-                        }
-                    ],
-                    'children': []
-                },
-                {
-                    'id': 5,
-                    'parentId': 3,
-                    'entities': [
-                        {
-                            'id': 9,
-                            'nodeId': 5,
-                            'name': 'Bb-1',
-                            'matchesCount': 1
-                        },
-                        {
-                            'id': 10,
-                            'nodeId': 5,
-                            'name': 'Bb-2',
-                            'matchesCount': 1
-                        }
-                    ],
-                    'children': []
-                }
-            ]
-        },
-        {
-            'id': 6,
-            'parentId': None,
-            'entities': [
-                {
-                    'id': 11,
-                    'nodeId': 6,
-                    'name': 'C-1',
-                    'matchesCount': 1
-                }
-            ],
-            'children': []
-        },
-        {
-            'id': 7,
-            'parentId': None,
-            'entities': [
-                {
-                    'id': 12,
-                    'nodeId': 7,
-                    'name': 'D-1',
-                    'matchesCount': 0
-                },
-                {
-                    'id': 13,
-                    'nodeId': 7,
-                    'name': 'D-2',
-                    'matchesCount': 0
-                }
-            ],
-            'children': []
-        }
-    ]
+    expected_node_ac = {
+        'id': 7,
+        'parentId': 0,
+        'entities': [
+            {'id': 12, 'nodeId': 7, 'name': 'Ac-1', 'matchesCount': 0}
+        ],
+        'children': []
+    }
+
+    expected_node_a = node_a.copy()
+    expected_node_a['children'].append(expected_node_ac)
+
+    expected_nodes = [expected_node_a, node_b, node_c]
 
     assert ordered(nodes) == ordered(expected_nodes)
-
-    DeepNodeSchema(many=True).load(nodes)
 
 
 def test_patch_node(client):
