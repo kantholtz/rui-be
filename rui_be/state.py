3d550d2ee5cd31184e32f758f3db954e38099ac6
Felix Hamann <felix@hamann.xyz>
2022-01-06 18:39:25 +0100

    disclaimer

--- rui_be/state.py
+++ rui_be/state.py
@@ -6,6 +6,10 @@ from draug.homag.model import Predictions
 from draug.homag.text import Matches
 
 
+# oh god please dont look at me and for the love of jesus christ,
+# do not deploy me properly (e.g. with wsgi)
+
+
 meta = None
 graphwriter = None
 graph = Graph(meta=dict(name="unknown"))
